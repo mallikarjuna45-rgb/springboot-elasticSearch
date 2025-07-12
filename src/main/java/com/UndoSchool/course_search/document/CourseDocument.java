bf81267cd545f400b38764566019323bc230d379
smallikarjuna45-rgb <smallikarjun713@gmail.com>
2025-07-12 11:56:53 +0530

    Creation of course documnet

---
+++ src/main/java/com/UndoSchool/course_search/document/CourseDocument.java
@@ -0,0 +1,46 @@
+package com.UndoSchool.course_search.document;
+
+import com.UndoSchool.course_search.enums.CourseType;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.Instant;
+
+@Data
+@Document(indexName = "courses")
+public class CourseDocument {
+
+    @Id
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String title;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Keyword)
+    private String category;
+
+    @Field(type = FieldType.Keyword)
+    private CourseType type;
+
+    @Field(type = FieldType.Keyword)
+    private String gradeRange;
+
+    @Field(type = FieldType.Integer)
+    private Integer minAge;
+
+    @Field(type = FieldType.Integer)
+    private Integer maxAge;
+
+    @Field(type = FieldType.Double)
+    private Double price;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_time)
+    private Instant nextSessionDate;
+}
---
+++ src/main/java/com/UndoSchool/course_search/enums/CourseType.java
@@ -0,0 +1,7 @@
+package com.UndoSchool.course_search.enums;
+
+public enum CourseType {
+    ONE_TIME,
+    COURSE,
+    CLUB
+}
